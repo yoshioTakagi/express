a25cc1b81473b2033b16f63b26620685b3d7a57e
yoshioTakagi <2301140043ze@cyber-u.ac.jp>
2025-06-09 21:01:55 +0900

    WebAPI NewsPaper

--- routes/notes.js
+++ routes/notes.js
@@ -1,23 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-
-// 接続情報を設定
-const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://user001:*******@cluster0.e0i5qgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+const request = require('request');
 
 router.get('/', async (req, res) => {
-// データベース、コレクションを指定
-const database = client.db('notes');
-const notes = database.collection('notes');
-
-// idが１のドキュメントを取得
-const query = { id: 1 };
-const note = await notes.findOne(query);
-
-res.json(note);
+request('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=622865d72d0b4597a52c17cca6f15f59', function (error, response, body) {
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
+});
 })
 
 module.exports = router;
-
